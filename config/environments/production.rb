88715463436e695019a2a71b69fda9589d58017c
Samuel McTaggart <sam.mctaggart@gmail.com>
2014-01-01 14:57:09 -0500

    initial ember setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Dogejack::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.ember.variant = :production
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,5 @@ Dogejack::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  config.ember.variant = :development
 end
