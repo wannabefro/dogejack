38af556b40b233bb1df52555589dabdf3d5a4296
Jay Byoun <jay@jaybyoun.com>
2014-01-08 20:07:53 -0500

    local db fix

--- app/assets/javascripts/controllers/game_controller.js
+++ app/assets/javascripts/controllers/game_controller.js
@@ -5,13 +5,13 @@ App.GameController = Ember.ObjectController.extend({
   canBet: function(){
     var balance = this.get('controllers.application.currentUser').get('wallets').get('content')[0].get('balance');
     if (balance > 0){
-      return true
+      return true;
     }
   }.property('state'),
 
   validBet: function(){
     var balance = this.get('controllers.application.currentUser').get('wallets').get('content')[0].get('balance');
-    if (parseInt(this.get('betAmount')) <= balance){
+    if (parseInt(this.get('betAmount'), 10) <= balance){
       this.get('controllers.application').set('errors', null);
       return true;
     } else {
@@ -41,7 +41,7 @@ App.GameController = Ember.ObjectController.extend({
     var that = this;
     if (this.get('state') === 'dealers_turn'){
       if (!this.dealersPlay){
-        this.dealersPlay = setInterval(function(){that.send('getDealersCard')}, 750);
+        this.dealersPlay = setInterval(function(){that.send('getDealersCard');}, 750);
       }
       return true;
     } else if (this.get('state') === 'finished') {
@@ -61,7 +61,7 @@ App.GameController = Ember.ObjectController.extend({
 
     deal: function(){
       var that = this;
-      if (this.get('bet') != 0){
+      if (this.get('bet') !== 0){
         this.set('betAmount', this.get('bet'));
       }
       data = this.getProperties('betAmount');
