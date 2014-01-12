9cf7307bf8e8754b8a6ef2c80053163d68526b5a
Jason Zopf <jasonzopf@gmail.com>
2014-01-12 12:45:35 -0500

    add cards

--- app/assets/javascripts/models/card.js
+++ app/assets/javascripts/models/card.js
@@ -2,4 +2,16 @@ App.Card = DS.Model.extend({
   value: DS.attr(),
   suit: DS.attr(),
   decks: DS.hasMany('deck'),
+
+  path: function() {
+    return "/assets/cards/" + this.get('cardString') +  ".svg"
+  }.property(),
+
+  cardString: function() {
+    return this.get('value') + this.get('suitLetter');
+  }.property(),
+
+  suitLetter: function() {
+    return {"♦": "D", "♠": "S", "♥": "H", "♣": "C"}[this.get('suit')];
+  }.property()
 });
